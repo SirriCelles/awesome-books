67bfba860fdeb1ca6add83c744652d3bb4dda6c4
SirriCelles <sirricelles@gmail.com>
2022-06-08 18:02:23 +0100

    fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { bookStore } from './modules/bookObj.js';
 import { loadHTML } from './modules/lists.js';
-import { DateTime } from './modules/luxon.min.js';
+import { DateTime } from './modules/luxon.js';
 
 const inputTitle = document.getElementById('book-title');
 const inputAuthor = document.getElementById('book-author');
@@ -20,7 +20,6 @@ const contactPage = document.querySelector('#contact-info-section');
 const listWrapper = document.getElementById('book-list-wrapper');
 
 document.querySelector('#dateTime').textContent = DateTime.now();
-console.log(DateTime.now());
 
 addLink.addEventListener('click', () => {
   listBookPage.classList.add('display-none');
